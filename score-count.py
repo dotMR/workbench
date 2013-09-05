4372fb77f75a6dde7b955cc56619f5fed84de864
mrusso <mrusso@mrusso-m1.local>
2013-09-05 22:57:44 +0200

    updated logic for score calculation

--- score-count.py
+++ score-count.py
@@ -11,16 +11,19 @@ with open ("resources/score-to-count.in", "r") as myfile:
     data=myfile.read().replace('\n', '')
 
 numMissing = 0
+denominator = 0
 numProcessed = 0
 totalScore = 0
 scoreToAdd = 0
 data = data.replace(" ","")
 for w in data.split(','):
 	numProcessed = numProcessed + 1
+	denominator = denominator + 1
 	scoreToAdd = 0
 	if ((("_DONT_KNOW" in w) or ("_MISSING" in w) or ("null" in w))):
 		numMissing = numMissing + 1
-	elif (("0pt" in w) or ("SCORE_ZERO" in w)):
+
+	if (("0pt" in w) or ("SCORE_ZERO" in w)):
 		scoreToAdd = 0
 	elif "_SCORE_ONE" in w:
 		scoreToAdd = 1
@@ -54,6 +57,10 @@ for w in data.split(','):
 		scoreToAdd = 0.8
 	elif "_0point9" in w:
 		scoreToAdd = 0.9
+	elif ((("_DONT_KNOW" in w) or ("_MISSING" in w) or ("null" in w))):
+		scoreToAdd = 0
+		denominator = denominator - 1
+		print 'not counting {}'.format(w)
 	else:
 		print 'no case found for {}'.format(w)
 	totalScore = totalScore + scoreToAdd
@@ -61,7 +68,8 @@ for w in data.split(','):
 numAnswered = numProcessed - numMissing
 
 print 'Missing: {0} of {1}\nTotal Points: {2}'.format(numMissing, numProcessed, totalScore)
+print 'dividing by {0} responses'.format(denominator)
 if numMissing > 13:
 	print 'Score: n/a'
 else:
-	print 'Score {0}'.format(round((totalScore/numAnswered)*100))
+	print 'Score {0}'.format(round((totalScore/denominator)*100))
